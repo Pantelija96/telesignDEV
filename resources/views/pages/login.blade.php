cfae93aea927b3af3e9d30f173b8d0cb470751f8
Pantelija96 <pantelijastosic@gmail.com>
2023-04-20 15:49:08 +0200

    login page with telesign logo

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -11,6 +11,7 @@
     <link href="{{asset('/')}}assets/fonts/inter/inter.css" rel="stylesheet" type="text/css">
     <link href="{{asset('/')}}assets/icons/phosphor/styles.min.css" rel="stylesheet" type="text/css">
     <link href="{{asset('/')}}assets/css/all.min.css" id="stylesheet" rel="stylesheet" type="text/css">
+    <link href="{{asset('/')}}assets/css/custom.css" id="stylesheet" rel="stylesheet" type="text/css">
     <!-- /global stylesheets -->
 
     <!-- Core JS files -->
@@ -25,7 +26,7 @@
 <body>
 
     <!-- Main navbar -->
-    <div class="navbar navbar-dark navbar-static py-2">
+    <div class="navbar navbar-static py-2" style="background-color: #202125;">
         <div class="container-fluid">
 
             <div class="d-flex justify-content-end align-items-center ms-auto">
@@ -63,7 +64,7 @@
                             <div class="card-body">
                                 <div class="text-center mb-3">
                                     <div class="d-inline-flex align-items-center justify-content-center mb-4 mt-2">
-                                        <img src="{{asset('/')}}assets/images/logo_icon.svg" class="h-48px" alt="">
+                                        <img src="{{asset('/')}}assets/img/telsignBlueLogo.png" class="h-48px" alt="">
                                     </div>
                                     <h3 class="mb-0">Login to your account</h3>
                                 </div>
@@ -89,7 +90,7 @@
                                 </div>
 
                                 <div class="mb-3">
-                                    <button type="submit" class="btn btn-primary w-100">Sign in</button>
+                                    <button type="submit" class="btn btn-telesign w-100">Sign in</button>
                                 </div>
 
                                 <div class="text-center">
